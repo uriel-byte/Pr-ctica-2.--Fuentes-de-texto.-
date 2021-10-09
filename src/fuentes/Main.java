1c0914f9f934177fad97c775749d7da1624285dd
Uriel Jacinto <71051834+uriel-byte@users.noreply.github.com>
2021-10-09 01:02:15 -0500

    Add files via upload

---
+++ src/fuentes/Main.java
@@ -0,0 +1,12 @@
+package fuentes;
+/**
+*  @web http://www.jc-mouse.net/
+ * @author Mouse
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new interfaz().setVisible(true);
+    }
+
+}
